8bdca4787bf78bc7790f35862caae4c0510e9fad
wojcikmichal <wojcikmichal@student.agh.edu.pl>
2021-12-30 15:56:59 +0100

    ulepszenie meni glownego i implementacja rejestracji klientow

--- Kod/Klient.cpp
+++ Kod/Klient.cpp
@@ -1,12 +1,18 @@
 #include <exception>
 #include <string>
+#include<fstream>
+
 using namespace std;
 
 #include "Klient.h"
 #include "Rezerwacja.h"
 
 void Klient::rejestracja(string imie, string nazwisko, string adres_email, string haslo, string numer_prawa_jazdy) {
-	throw "Not yet implemented";
+	ofstream zapis("baza_klientow.txt");
+
+	zapis << imie << endl << nazwisko << endl << adres_email << endl << haslo << endl << numer_prawa_jazdy << endl;
+
+	zapis.close();
 }
 
 void Klient::zaplac(Rezerwacja rezerwacja) {
--- Kod/main.cpp
+++ Kod/main.cpp
@@ -2,42 +2,11 @@
 #include <regex>
 #include <string>
 
+#include "Klient.h"
+
 using namespace std;
 
-void przegladanie_samochodow();
-void rejestracja();
-
-bool is_email_valid(const string& email)
-{
-	// define a regular expression
-	const regex pattern
-	("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
-
-	// try to match the string with the regular expression
-	return regex_match(email, pattern);
-}
-
-void menu_start() {
-	system("CLS");
-	int wybor;
-	cout << "Witaj w wypozyczalni Ultra Flota" << endl;
-	cout << "Wybierz co chcesz zrobic." << endl;
-	cout << "1. Przegladaj samochody." << endl;
-	cout << "2. Zarejestruj sie." << endl;
-	cout << "3. Zmien termin najmu." << endl;
-	cout << "4. Odbierz samochod. (Wymagane konto pracownika)" << endl;
-	cout << "5. Wydaj samochod. (Wymagane konto pracownika)" << endl;
-	cout << "6. Zarzadzaj baza pojazdow. (Wymagane konto menedzera)" << endl;
-	cout << "Wpisz liczbe: ";
-	cin >> wybor;
-
-	if (wybor == 1) {
-		przegladanie_samochodow();
-	}
-	else if (wybor == 2) {
-		rejestracja();
-	}
-}
+void menu_start();
 
 void przegladanie_samochodow() {
 	system("CLS");
@@ -45,9 +14,11 @@ void przegladanie_samochodow() {
 	cout << "0. Powrot." << endl;
 	cout << "Aby zarezerwowac samochod wpisz jego numer" << endl;
 	cout << "Lista dostepnych samochodow:" << endl;
+
 	//tu trzeba zrobic to wczytywanie z pliku
 	cout << "1. Fiesta" << endl;
 	cout << "2. Opelek" << endl;
+
 	cout << "Wybor: ";
 	cin >> a;
 	if (a == 0) {
@@ -55,10 +26,19 @@ void przegladanie_samochodow() {
 	}
 }
 
-void rejestracja() {
+bool czy_email_poprawny(const string& email){
+	
+	// define a regular expression
+	const regex wzor ("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+
+	// try to match the string with the regular expression
+	return regex_match(email, wzor);
+}
+
+void formularz_rejestracji() {
 	system("CLS");
 	int wybor;
-	cout << "0. Powrot"<< endl;
+	cout << "0. Powrot" << endl;
 	cout << "1. Formularz rejestracji" << endl;
 Wybor:
 	cout << "Wybor: ";
@@ -72,6 +52,7 @@ Wybor:
 		string email;
 		string nr_prawa_jazdy;
 		string haslo;
+
 		cout << "Wpisz imie:" << endl;
 		cin >> imie;
 		cout << endl;
@@ -81,13 +62,19 @@ Wybor:
 	Email: //tu cos nie gra bo od razu wywala ze bledny email i potem juz dziala dobrze
 		cout << "Wpisz email:" << endl;
 		cin >> email;
-		if (is_email_valid(email)) {
+		if (czy_email_poprawny(email)) {
 			cout << "Wpisz numer prawa jazdy:" << endl;
 			cin >> nr_prawa_jazdy;
 			cout << endl;
-			cout << "Wprowadz haslo:"<<endl;
+			cout << "Wprowadz haslo:" << endl;
 			cin >> haslo;
-			//i tu konstruktor klient(tu dane)
+			
+			Klient nowy_klient;
+			nowy_klient.rejestracja(imie, nazwisko, email, haslo, nr_prawa_jazdy);
+
+			system("cls");
+			cout << "Poprawnie zarejestrowano uzytkownika " << imie << " " << nazwisko << endl;
+			system("pause");
 		}
 		else {
 			cout << "Niepoprawny adres email. Sprobuj ponownie." << endl;
@@ -97,6 +84,57 @@ Wybor:
 	else {
 		cout << "Wybrano zla wartosc. Sprobuj ponownie." << endl;
 		goto Wybor;
+	}
+
+	menu_start();
+}
+
+void menu_start() {
+	system("CLS");
+	int wybor;
+	cout << "Witaj w wypozyczalni Ultra Flota" << endl;
+	cout << "Wybierz co chcesz zrobic." << endl;
+	cout << "1. Przegladaj samochody." << endl;
+	cout << "2. Zarejestruj sie." << endl;
+	cout << "3. Zmien termin najmu." << endl;
+	cout << "4. Odbierz samochod. (Wymagane konto pracownika)" << endl;
+	cout << "5. Wydaj samochod. (Wymagane konto pracownika)" << endl;
+	cout << "6. Zarzadzaj baza pojazdow. (Wymagane konto menedzera)" << endl;
+	cout << "7. Wyjdz." << endl;
+	cout << "Wybierz opcje (podajac jej numer): ";
+	cin >> wybor;
+
+	switch (wybor)
+	{
+	case 1:
+		przegladanie_samochodow();
+		break;
+
+	case 2:
+		formularz_rejestracji();
+		break;
+
+	case 3:
+		//jakiś kod
+		break;
+
+	case 4:
+		//jakiś kod
+		break;
+
+	case 5:
+		//jakiś kod
+		break;
+
+	case 6:
+		//jakiś kod
+		break;
+	case 7:
+		exit;
+		break;
+	default:
+		cout << "Wybrano nieprawidlowa opcje!" << endl;
+		break;
 	}
 }
 
